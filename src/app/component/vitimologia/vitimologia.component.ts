4be74493531300466c779c6449c0f2ed815a36e2
FABIANO <fabiano.leite.freitas@gmail.com>
2024-07-23 17:21:31 -0300

    commit cadastro motorista

--- src/app/component/vitimologia/vitimologia.component.ts
+++ src/app/component/vitimologia/vitimologia.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
+import { StepperModule } from 'primeng/stepper';
+import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
+import { CommonModule } from '@angular/common';
+import { InputTextModule } from 'primeng/inputtext';
+import { FormsModule } from '@angular/forms';
+import { DropdownModule } from 'primeng/dropdown';
+
+
 
 @Component({
   selector: 'app-vitimologia',
   standalone: true,
-  imports: [],
+  imports: [ButtonModule, StepperModule,CardModule,CommonModule,InputTextModule,FormsModule,DropdownModule],
   templateUrl: './vitimologia.component.html',
   styleUrl: './vitimologia.component.scss'
 })
 export class VitimologiaComponent {
+  value: string ='';
+
 
 }
+
