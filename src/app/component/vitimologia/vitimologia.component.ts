2718d928a75da937a8dc5b8ef6d0aaf850e8cb89
FABIANO <fabiano.leite.freitas@gmail.com>
2024-07-24 14:24:52 -0300

    commit controle do cliente

--- src/app/component/vitimologia/vitimologia.component.ts
+++ src/app/component/vitimologia/vitimologia.component.ts
@@ -7,28 +7,78 @@ import { InputTextModule } from 'primeng/inputtext';
 import { FormsModule } from '@angular/forms';
 import { DropdownModule } from 'primeng/dropdown';
 
-
-
 @Component({
   selector: 'app-vitimologia',
   standalone: true,
-  imports: [ButtonModule, StepperModule,CardModule,CommonModule,InputTextModule,FormsModule,DropdownModule],
-  templateUrl: './vitimologia.component.html',
-  styleUrl: './vitimologia.component.scss'
+  imports: [
+    ButtonModule,
+    StepperModule,
+    CardModule,
+    CommonModule,
+    InputTextModule,
+    FormsModule,
+    DropdownModule
+  ],
+  templateUrl: './vitimologia.component.html', 
+  styleUrls: ['./vitimologia.component.scss']
 })
 export class VitimologiaComponent {
-  
-  value: string ='';
+  fullName: string = '';
   selectedGender: any;
+  date: string = '';
+  cpf: string = '';
+  rg: string = '';
+  dataEmissaoRg: string = '';
+  orgaoExp: string = '';
+  controleCliente: string = ''; // Novo campo para Controle do Cliente
   genders: any[] = [
     { label: 'Masculino', value: 'M' },
     { label: 'Feminino', value: 'F' }
   ];
 
-
+  onDateInput(event: Event) {
+    const input = event.target as HTMLInputElement;
+    let value = input.value.replace(/\D/g, ''); // Remove todos os caracteres não numéricos
+    if (value.length > 2) {
+      value = value.slice(0, 2) + '/' + value.slice(2);
+    }
+    if (value.length > 5) {
+      value = value.slice(0, 5) + '/' + value.slice(5, 9);
+    }
+    input.value = value;
+    this.date = value;
   }
-  
 
+  onCpfInput(event: Event) {
+    const input = event.target as HTMLInputElement;
+    let value = input.value.replace(/\D/g, ''); // Remove todos os caracteres não numéricos
+    if (value.length > 11) {
+      value = value.slice(0, 11); // Limita a 11 dígitos
+    }
+    input.value = value;
+    this.cpf = value;
+  }
 
+  onRgInput(event: Event) {
+    const input = event.target as HTMLInputElement;
+    let value = input.value.replace(/\D/g, ''); // Remove todos os caracteres não numéricos
+    input.value = value;
+    this.rg = value;
+  }
+
+  onDataEmissaoRgInput(event: Event) {
+    const input = event.target as HTMLInputElement;
+    let value = input.value.replace(/\D/g, ''); // Remove todos os caracteres não numéricos
+    if (value.length > 2) {
+      value = value.slice(0, 2) + '/' + value.slice(2);
+    }
+    if (value.length > 5) {
+      value = value.slice(0, 5) + '/' + value.slice(5, 9);
+    }
+    input.value = value;
+    this.dataEmissaoRg = value;
+  }
 }
+
+
 
