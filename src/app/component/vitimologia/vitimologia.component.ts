b8d19b9838c0950b6130bf774f63cc9f3e45a90c
FABIANO <fabiano.leite.freitas@gmail.com>
2024-07-25 17:54:27 -0300

    adicionado todas as labels

--- src/app/component/vitimologia/vitimologia.component.ts
+++ src/app/component/vitimologia/vitimologia.component.ts
@@ -25,17 +25,42 @@ import { DropdownModule } from 'primeng/dropdown';
 export class VitimologiaComponent {
   fullName: string = '';
   selectedGender: any;
+  selecionefuncao: any;
+  selecioneperfil: any;
   date: string = '';
   cpf: string = '';
+  CNPJ: string = '';
   rg: string = '';
   dataEmissaoRg: string = '';
   orgaoExp: string = '';
-  controleCliente: string = ''; // Novo campo para Controle do Cliente
+  controleCliente: string = '';
+  motherName: string = ''; // Novo campo para Nome da Mãe
+  fatherName: string = ''; // Novo campo para Nome do Pai
+
   genders: any[] = [
     { label: 'Masculino', value: 'M' },
     { label: 'Feminino', value: 'F' }
   ];
 
+  funcoes: any[] = [
+    {label: 'Motorista', value: 'Motorista'},
+    {label: 'Ajudante', value: 'Ajudante'},
+    {label: 'Outros', value: 'Outros'},
+    {label: 'Vigilante', value: 'Vigilante'},
+    {label: 'DSP', value: 'DSP'},
+    {label: 'HDP Owner', value: 'HDP Owner'},
+    {label: 'HDP DA Driver', value: 'HDP DA Driver'},
+    {label: 'HDP DA Non Driver', value: 'HDP DA Non Driver'},
+    
+  ];
+
+  perfil: any[] = [
+    {label: 'Frota', value: 'Frota'},
+    {label: 'Agregado', value: 'Agregado'},
+    {label: 'Autônomo', value: 's'},
+  ];
+
+  
   onDateInput(event: Event) {
     const input = event.target as HTMLInputElement;
     let value = input.value.replace(/\D/g, ''); // Remove todos os caracteres não numéricos
@@ -78,7 +103,26 @@ export class VitimologiaComponent {
     input.value = value;
     this.dataEmissaoRg = value;
   }
+
+    onCnpjInput(event: Event) {
+    const input = event.target as HTMLInputElement;
+    let value = input.value.replace(/\D/g, ''); // Remove todos os caracteres não numéricos
+    if (value.length > 14) {
+      value = value.slice(0, 14); // Limita a 14 dígitos
+    }
+    if (value.length > 2) {
+      value = value.slice(0, 2) + '.' + value.slice(2);
+    }
+    if (value.length > 6) {
+      value = value.slice(0, 6) + '.' + value.slice(6);
+    }
+    if (value.length > 10) {
+      value = value.slice(0, 10) + '/' + value.slice(10);
+    }
+    if (value.length > 15) {
+      value = value.slice(0, 15) + '-' + value.slice(15);
+    }
+    input.value = value;
+    this.CNPJ = value;
+  }
 }
-
-
-
